670105a5273369236ddc38cb06299c37efe8eba3
Jonathan Shyaka <50402526+drayzii@users.noreply.github.com>
2019-07-10 14:55:19 +0200

    [chore 167194110] Add validation on to requests

--- server/routes/propertyRoute.js
+++ server/routes/propertyRoute.js
@@ -1,6 +1,7 @@
 import { Router as expressRouter } from 'express';
 import fileupload from 'express-fileupload';
 import propertyController from '../controller/propertyController';
+import validation from '../middleware/validation';
 
 const router = expressRouter();
 
@@ -8,11 +9,11 @@ router.use(fileupload({
   useTempFiles: true,
 }));
 
-router.post('/', propertyController.addProperty);
+router.post('/', validation.propertyValidation, propertyController.addProperty);
 router.delete('/:id', propertyController.deleteProperty);
 router.get('/:id', propertyController.viewSpecificProperty);
 router.get('/', propertyController.viewAllProperties);
-router.patch('/:id', propertyController.updateProperty);
+router.patch('/:id', validation.propertyValidation, propertyController.updateProperty);
 router.patch('/:id/sold', propertyController.markAsSold);
 
 export default router;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import { Router as expressRouter } from 'express';
 import userController from '../controller/userController';
+import validation from '../middleware/validation';
 
 const router = expressRouter();
 
-router.post('/signup', userController.signUp);
-router.post('/signin', userController.signIn);
+router.post('/signup', validation.userValidation, userController.signUp);
+router.post('/signin', validation.emailValidation, validation.passwordValidation, userController.signIn);
 
 export default router;
